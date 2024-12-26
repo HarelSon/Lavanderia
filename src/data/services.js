88006a2381e7670ee5c11dfdc565f41b9dee9bed
luz yucra <luzyucra0@gmail.com>
2024-12-26 14:35:47 -0500

    Add base code.

---
+++ src/data/services.js
@@ -0,0 +1,42 @@
+export const services = [
+    {
+      id: 1,
+      title: "Lavado de ternos",
+      description: "Ternos, camisas, pantalones, blusas y mucho más, todas tus prendas pasarán por un control de calidad premium."
+    },
+    {
+      id: 2,
+      title: "Lavado de vestidos de novia",
+      description: "Con la certificación Wedding Gown Specialist podemos asegurar que tu vestido de novia recibirá los mejores cuidados."
+    },
+    {
+      id: 3,
+      title: "Lavado de zapatillas",
+      description: "Tus zapatillas en las manos de especialistas que tienen años de experiencia brindando un servicio A1, piénsalo."
+    },
+    {
+      id: 4,
+      title: "Lavado de ropa de cama",
+      description: "Servicio de lavado de ropa de cama, incluyendo sábanas, fundas, colchas y edredones, con limpieza profunda, eliminación de manchas y bacterias, secado y planchado."
+    },
+    {
+      id: 5,
+      title: "Lavado de cortinas",
+      description: "Limpieza profunda, eliminación de manchas, secado y planchado, garantizando frescura y cuidado de los tejidos. Ideal para hogares y oficinas."
+    },
+    {
+      id: 6,
+      title: "Lavado de mantelería",
+      description: "Eliminación de manchas difíciles, secado especializado y planchado, garantizando una presentación impecable. Perfecto para eventos, restaurantes y establecimientos comerciales."
+    },
+    {
+      id: 7,
+      title: "Lavado de Vestidos de Fiesta",
+      description: "Cuidado especializado para vestidos de fiesta, eliminando manchas y preservando la tela, con lavado delicado, secado controlado y planchado preciso para mantener su forma y elegancia."
+    },
+    {
+      id: 8,
+      title: "Lavado de peluches",
+      description: "Limpieza suave y profunda de peluches, eliminando suciedad y olores, con secado cuidadoso para preservar su suavidad y forma original, asegurando un toque fresco y agradable."
+    },
+];
