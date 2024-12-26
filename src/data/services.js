72d794260479da0e893f183b4fb971d845f8576e
luz yucra <luzyucra0@gmail.com>
2024-12-26 16:57:51 -0500

    Updated services file.

--- src/data/services.js
+++ src/data/services.js
@@ -1,9 +1,22 @@
-export const services = [
+export const services = {
+  primary: [
     {
       id: 1,
       title: "Lavado de ternos",
       description: "Ternos, camisas, pantalones, blusas y mucho más, todas tus prendas pasarán por un control de calidad premium."
     },
+    {
+      id: 4,
+      title: "Lavado de ropa de cama",
+      description: "Servicio de lavado de ropa de cama, incluyendo sábanas, fundas, colchas y edredones, con limpieza profunda, eliminación de manchas y bacterias, secado y planchado."
+    },
+    {
+      id: 7,
+      title: "Lavado de Vestidos de Fiesta",
+      description: "Cuidado especializado para vestidos de fiesta, eliminando manchas y preservando la tela, con lavado delicado, secado controlado y planchado preciso para mantener su forma y elegancia."
+    },
+  ],
+  secondary: [
     {
       id: 2,
       title: "Lavado de vestidos de novia",
@@ -14,11 +27,7 @@ export const services = [
       title: "Lavado de zapatillas",
       description: "Tus zapatillas en las manos de especialistas que tienen años de experiencia brindando un servicio A1, piénsalo."
     },
-    {
-      id: 4,
-      title: "Lavado de ropa de cama",
-      description: "Servicio de lavado de ropa de cama, incluyendo sábanas, fundas, colchas y edredones, con limpieza profunda, eliminación de manchas y bacterias, secado y planchado."
-    },
+    
     {
       id: 5,
       title: "Lavado de cortinas",
@@ -29,14 +38,11 @@ export const services = [
       title: "Lavado de mantelería",
       description: "Eliminación de manchas difíciles, secado especializado y planchado, garantizando una presentación impecable. Perfecto para eventos, restaurantes y establecimientos comerciales."
     },
-    {
-      id: 7,
-      title: "Lavado de Vestidos de Fiesta",
-      description: "Cuidado especializado para vestidos de fiesta, eliminando manchas y preservando la tela, con lavado delicado, secado controlado y planchado preciso para mantener su forma y elegancia."
-    },
+    
     {
       id: 8,
       title: "Lavado de peluches",
       description: "Limpieza suave y profunda de peluches, eliminando suciedad y olores, con secado cuidadoso para preservar su suavidad y forma original, asegurando un toque fresco y agradable."
     },
-];
+  ]
+}
